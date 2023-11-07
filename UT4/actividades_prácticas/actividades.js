02093ad595abb7e8c0e256fcbc27c83be458c2a7
Nuhazet Correa Torres <nuhazet6@gmail.com>
2023-11-07 18:34:22 +0000

    pendiente actividad 4

--- UT4/actividades_prácticas/actividades.js
+++ UT4/actividades_prácticas/actividades.js
@@ -10,8 +10,34 @@ function random_array(arr_length=10,min_value=100,max_value=200){
 //Actividad 2 y 3
 function random_array_sorted(arr_length=20,min_value=20,max_value=100,disorder=false){
     disorder ? disor_func = (a,b) => Math.floor(Math.random()*(arr_length+1)) : disor_func=(a,b) => a-b;
-    arr_rnd = random_array(arr_length,min_value,max_value)
-    arr_rnd.sort()
+    arr_rnd = random_array(arr_length,min_value,max_value);
     return arr_rnd.sort(disor_func);
 }
-//Actividad 3
+//Actividad 4
+function actividad_4(arr_length=15,min_value=-10,max_value=20){
+    arr_rnd = random_array(arr_length,min_value,max_value);
+    arr_rnd.forEach(x => {
+        switch (true) {
+            case x <= -5:
+                let description = "Eliminar al principio"                
+                number = arr_rnd[0]
+                break;
+            case -5 < x <= 0:
+                let description = "Eliminar al último"                
+                number = arr_rnd[arr_rnd.length-1]
+                break;
+            case 0 < x <= 10:
+                let description = "Añadir al principio"                
+            
+            break;
+            case 10 < x <= 20:
+                let description = "Añadir al final"                
+            
+            break;
+        }
+        new_array = [].concat(arr_rnd)
+        eval(`new_array.`)
+        
+    });
+
+}
